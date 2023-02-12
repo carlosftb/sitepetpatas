51408a4267d6bd3c83cd68871a64e4e02aa684ef
Carlos Silva <carlos.david.ftb@hotmail.com>
2023-02-12 12:39:04 -0300

    Ajustando template para o negocio

--- petpatas/urls.py
+++ petpatas/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from petpatas.views import index
+from petpatas.views import index, service, about, findus
+
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='index'),
+    path('service/', service, name='service'),
+    path('about/', about, name='about'),
+    path('findus/', findus, name='findus'),
 
 ]
--- petpatas/views.py
+++ petpatas/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
 
-
 def index(request):
-    return render(request, 'petpatas/index.html')
+    return render(request, "petpatas/index.html")
+
+def service(request):
+    return render(request, "petpatas/services.html")
+
+def about(request):
+    return render(request, "petpatas/about.html")
+
+def findus(request):
+    return render(request, "petpatas/findus.py")
