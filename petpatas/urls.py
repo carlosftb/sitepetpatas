d06a58877323372de5685c920b61a52996e2c223
Carlos Silva <carlos.david.ftb@hotmail.com>
2023-03-18 23:56:00 -0300

    Adicionando o map e ajustando pagina about

--- petpatas/urls.py
+++ petpatas/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
 from petpatas.views import index, service, about, findus
 
-
 urlpatterns = [
     path('', index, name='index'),
     path('service/', service, name='service'),
     path('about/', about, name='about'),
     path('findus/', findus, name='findus'),
-    
 
 ]
